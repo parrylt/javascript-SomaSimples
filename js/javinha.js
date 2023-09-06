8d6368a91858437d9536eae1074e761facbe0c3e
Rômulo Junior <132522416+parrylt@users.noreply.github.com>
2023-09-06 00:07:46 -0300

    Add files via upload

---
+++ js/javinha.js
@@ -0,0 +1,34 @@
+﻿// cmt
+/*
+
+
+*/
+
+var nome = "Marina Coral"; //String
+var idade = 29; //int
+var altura = 1.68; //float ou double
+var filhos = false; //Boolean
+
+/* //alert ("Nome: " + nome);
+//document.write("Idade: " + idade); */
+
+
+//prompt lê somente texto, é necessario converter para a variavel desejada
+var n1 = parseFloat(prompt("Digite um valor: ")); 
+
+//parse converte para outra tipo de variavel 
+var n2 = parseFloat(prompt("Digite outro valor: "));
+
+var total = n1+n2;
+
+document.write("Total: " + total);
+
+if (total >= 20 || total==30)
+{
+	document.write("<br>" + "Valor total é maior ou igual à 20!");
+}
+
+else
+{
+	document.write("<br>" + "Valor total é menor ou igual à 20!");
+}
